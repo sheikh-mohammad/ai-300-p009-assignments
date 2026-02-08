3f7b00acb6daf4e4b9e1f4ed1aae1bf7539ff1ef
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-02-09 00:20:32 +0500

    Completed question 03

---
+++ assignment_03/chapter_21/lesson_05/exercise_01/question_03/main.py
@@ -0,0 +1,8 @@
+# 3. Convert to uppercase: ['HELLO', 'WORLD'] from ['hello', 'world']
+
+lower_cased_words : list[str] = ['hello', 'world']
+
+upper_cased_words : list[str] = [word.upper() for word in lower_cased_words]
+
+print(f"Lower Cased Words: {lower_cased_words}")
+print(f"Upper Cased Words: {upper_cased_words}")
