4a57c1d186694c5b00dcee8971c7f199ec22bc7d
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-02-09 00:35:03 +0500

    Completed question 01 of exercise 03 of lesson 05 of chapter 21

---
+++ assignment_03/chapter_21/lesson_05/exercise_03/question_01/main.py
@@ -0,0 +1,9 @@
+# Exercise 3: Transform and Filter
+# Square only odd numbers: From [1, 2, 3, 4, 5], get [1, 9, 25]
+
+numbers : list[int] = [1, 2, 3, 4, 5]
+
+odd_squared_nums : list[int] = [num ** 2 for num in numbers if num % 2 != 0]
+
+print(f"Numbers: {numbers}")
+print(f"Odd Squared Numbers: {odd_squared_nums}")
