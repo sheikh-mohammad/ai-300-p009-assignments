a18338674e27cd0e510e0880e6bdf1bd79c90151
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-02-09 00:21:31 +0500

    Added comments

--- assignment_03/chapter_21/lesson_05/exercise_01/question_01/main.py
+++ assignment_03/chapter_21/lesson_05/exercise_01/question_01/main.py
@@ -1,3 +1,5 @@
+# 1. Double each number: [2, 4, 6, 8, 10] from [1, 2, 3, 4, 5]
+
 numbers : list[int] = [1, 2, 3, 4, 5]
 
 squared_numbers : list[int] = [num ** 2 for num in numbers]
--- assignment_03/chapter_21/lesson_05/exercise_01/question_02/main.py
+++ assignment_03/chapter_21/lesson_05/exercise_01/question_02/main.py
@@ -1,3 +1,5 @@
+# 2. Extract first letters: ['a', 'b', 'c'] from ['apple', 'banana', 'cherry']
+
 fruits : list[str] = ['apple', 'banana', 'cherry']
 
 first_letters : list[str] = [fruit[0] for fruit in fruits]
